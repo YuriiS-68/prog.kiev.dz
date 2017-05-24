100d15db3d23ddac9c7e2056588730439330cea1
YuriiS-68 <yurii_s@bigmir.net>
2017-05-24 15:46:27 +0300

    Changes were made to the file ExampleThreeClass

---
+++ src/prog/kiev/dz/strings/ExampleFourClass.java
@@ -0,0 +1,12 @@
+package prog.kiev.dz.strings;
+
+/**
+ * Created by Skorodielov on 24.05.2017.
+ */
+public class ExampleFourClass {
+    public static void main(String[] args) {
+
+        String str = "The Java programming language is a relatively high-level language, in that details of the machine representation are not available through the language.";
+
+    }
+}
--- src/prog/kiev/dz/strings/ExampleThreeClass.java
+++ src/prog/kiev/dz/strings/ExampleThreeClass.java
@@ -9,31 +9,28 @@ import java.util.Random;
 public class ExampleThreeClass {
     public static void main(String[] args) {
 
-        Random rnd = new Random();
         int massLenght = 1000000;
         int count = 0;
 
         String [] massStrok = new String[massLenght];
 
         for (int i = 0; i < massLenght; i++) {
-            StringBuilder str = new StringBuilder();
-            int strLenght = random(10, 1000, rnd);
-            for (int j = 0; j <= strLenght; j++) {
-                char symbol = (char)((int)randomSymbol(65, 122, rnd));
-                str.append(symbol);
+            char[] str = new char[random(10, 1000)];
+            for (int j = 0; j <= str.length; j++) {
+                char symbol = (char)((int)randomSymbol(65, 122));
             }
-            massStrok[i] = str.toString();
+            massStrok[i] = new String(str);
             count += massStrok[i].length();
         }
         System.out.println(count);
     }
 
-    public static int random(int start, int end, Random rnd){
+    public static int random(int start, int end){
         end -= start;
         return (int) (Math.random() * ++end) + start;
     }
 
-    public static int randomSymbol(int start, int end, Random rnd){
+    public static int randomSymbol(int start, int end){
         end -= start;
         return (int) (Math.random() * ++end) + start;
     }
